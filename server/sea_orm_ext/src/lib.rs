f0fd783eb066a97ee374c21a9a8be9e48c2c02c6
studylessshape <a664554724@qq.com>
2025-05-16 14:15:16 +0800

    chore: add doc for `InsertModelTrait` and `InsertModel`

--- server/sea_orm_ext/src/lib.rs
+++ server/sea_orm_ext/src/lib.rs
@@ -7,6 +7,52 @@ pub enum InsertValue<T> {
     Default,
 }
 
+/// It is recommended to implement using `#[derive(Clone, InsertModel)]`. But also can implement manually.
+/// 
+/// ```rust
+/// mod user {
+///     use sea_orm::{entity::prelude::*, FromQueryResult, Statement};
+///     use sea_orm_ext::InsertModelTrait;
+/// 
+///     #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+///     #[sea_orm(table_name = "user")]
+///     pub struct Model {
+///         #[sea_orm(primary_key)]
+///         pub id: i32,
+///         pub name: String,
+///         #[sea_orm(default = 1)]
+///         pub status: i32,
+///     }
+/// 
+///     #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+///     pub enum Relation {}
+///     impl ActiveModelBehavior for ActiveModel {}
+/// 
+///     #[derive(Debug, Clone)]
+///     pub struct InsertModel {
+///         pub id: i32,
+///         pub name: String,
+///     }
+/// 
+///     impl InsertModelTrait for InsertModel  {
+///         type Model = Model;
+///         async fn insert(&self, conn: &DatabaseConnection) -> Result<Self::Model, sea_orm::DbErr> {
+///             let model = user::Model::find_by_statement(
+///                 Statement::from_sql_and_values(
+///                     conn.get_database_backend(),
+///                     "INSERT INTO sys_user (id, name) VALUES($1,$2) REUTRNING *;",
+///                     [self.id.into(), self.name.clone().into()]
+///                 )
+///             ).one(conn).await?;
+///             if let Some(model) = model {
+///                 Ok(model)
+///             } else {
+///                 Err(sea_orm::DbErr::Exec(sea_orm::RuntimeErr::Internal("Insert User Failed.".to_string())))
+///             }
+///         }
+///     }
+/// }
+/// ```
 pub trait InsertModelTrait {
     type Model: ModelTrait;
     fn insert(&self, conn: &DatabaseConnection) -> impl std::future::Future<Output = Result<Self::Model, sea_orm::DbErr>> + Send;
@@ -42,19 +88,5 @@ mod test {
             pub id: i32,
             pub name: String,
         }
-
-        // impl InsertModelTrait for InsertModel  {
-        //     type Model = Model;
-        //     async fn insert(&self, conn: &DatabaseConnection) -> Result<Self::Model, sea_orm::DbErr> {
-        //         let model = user::Model::find_by_statement(Statement::from_sql_and_values(conn.get_database_backend(), r#"
-        //             INSERT INTO sys_user (id, name) VALUES($1,$2) REUTRNING *;
-        //         "#, [self.id.into(), self.name.clone().into()])).one(conn).await?;
-        //         if let Some(model) = model {
-        //             Ok(model)
-        //         } else {
-        //             Err(sea_orm::DbErr::Exec(sea_orm::RuntimeErr::Internal("Insert User Failed.".to_string())))
-        //         }
-        //     }
-        // }
     }
 }
--- server/sea_orm_ext_macro/src/lib.rs
+++ server/sea_orm_ext_macro/src/lib.rs
@@ -2,6 +2,60 @@ use proc_macro::TokenStream;
 use syn::{parse_macro_input, Data, DeriveInput, Error, Fields, Path};
 use quote::quote;
 use proc_macro2::TokenStream as TokenStream2;
+
+/// Derives the `InsertModel` trait for the given struct.
+/// 
+/// ## Examples
+/// 
+/// Example entity definition:
+/// 
+/// ```rust
+/// mod user {
+///     use sea_orm::{entity::prelude::*, FromQueryResult, Statement};
+///     use sea_orm_ext::InsertModel;
+///     #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+///     #[sea_orm(table_name = "user")]
+///     pub struct Model {
+///         #[sea_orm(primary_key)]
+///         pub id: i32,
+///         pub name: String,
+///         #[sea_orm(default = 1)]
+///         pub status: i32,
+///     }
+/// 
+///     #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+///     pub enum Relation {}
+/// 
+///     impl ActiveModelBehavior for ActiveModel {}
+/// }
+/// ```
+/// 
+/// Define insert model in sample mod scope:
+/// 
+/// ```
+/// mod user {
+///     // ...Model code
+/// 
+///     #[derive(Clone, InsertModel)]
+///     pub struct NewUser {
+///         pub id: i32,
+///         pub name: String,
+///     }
+/// }
+/// ```
+/// 
+/// Or define in other mod scope:
+/// 
+/// ```rust
+/// mod other {
+///     #[derive(Clone, InsertModel)]
+///     #[sea_orm_ext(mod = path_to::user)]
+///     pub struct NewUser {
+///         pub id: i32,
+///         pub name: String,
+///     }
+/// }
+/// ```
 #[proc_macro_derive(InsertModel, attributes(sea_orm_ext))]
 pub fn derive_insert_model(input: TokenStream) -> TokenStream {
     let DeriveInput {
